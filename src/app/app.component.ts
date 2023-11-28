58e5c38b05e5a979cac9d892374b866d9f2feaab
Christian <ch.apablaza@duocuc.cl>
2023-11-28 20:13:16 -0300

    Se agrega el proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular'; 
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Inicio', url: '/inicio', icon: 'home' },
+  ];
+
+  constructor(public router: Router, private menu: MenuController)  { }
+
+
+  abrirMapa() {
+    this.router.navigate(["/mapa"]);
+    this.menu.close();
+  }
+
+  cerrarSesion(){
+    localStorage.removeItem('ingresado');
+    this.router.navigate(["/ingreso"]);
+    this.menu.close();
+  }
+}
